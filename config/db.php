d0cf5741f2cc915dc2fec7c776acfa14d228b9c3
kitsabrahams <kitsabrahams@gmail.com>
2015-09-23 11:06:46 +0300

    database connection2

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=195.168.20.5;dbname=thirdparty_payments_db', 
+    'dsn' => 'pgsql:host=192.168.20.5;dbname=thirdparty_payments_db', 
     'username' => 'sandbox',
     'password' => 'sandbox',
     'charset' => 'utf8',
@@ -13,3 +13,4 @@ return [
       ]
     ], // PostgreSQL
 ];
+
